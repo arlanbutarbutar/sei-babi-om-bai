b45b435a46732ae65a1bc071621efaf67718074c
arlanbutarbutar <arlan270899@gmail.com>
2023-03-15 10:21:58 +0800

    system login on progress

---
+++ controller/functions.php
@@ -0,0 +1,104 @@
+
+<?php
+if (!isset($_SESSION["data-user"])) {
+  function masuk($data)
+  {
+    global $conn;
+    $email = htmlspecialchars(addslashes(trim(mysqli_real_escape_string($conn, $data["email"]))));
+    $password = htmlspecialchars(addslashes(trim(mysqli_real_escape_string($conn, $data["password"]))));
+
+    // check account
+    $checkAccount = mysqli_query($conn, "SELECT * FROM users WHERE email='$email'");
+    if (mysqli_num_rows($checkAccount) == 0) {
+      $_SESSION["message-danger"] = "Maaf, akun yang anda masukan belum terdaftar.";
+      $_SESSION["time-message"] = time();
+      return false;
+    } else if (mysqli_num_rows($checkAccount) > 0) {
+      $row = mysqli_fetch_assoc($checkAccount);
+      if (password_verify($password, $row["password"])) {
+        $_SESSION["data-user"] = [
+          "id" => $row["id_user"],
+          "role" => $row["id_role"],
+          "email" => $row["email"],
+          "username" => $row["username"],
+        ];
+      } else {
+        $_SESSION["message-danger"] = "Maaf, kata sandi yang anda masukan salah.";
+        $_SESSION["time-message"] = time();
+        return false;
+      }
+    }
+  }
+  function daftar($data)
+  {
+    global $conn;
+    $username = htmlspecialchars(addslashes(trim(mysqli_real_escape_string($conn, $data['username']))));
+    $email = htmlspecialchars(addslashes(trim(mysqli_real_escape_string($conn, $data['email']))));
+    $password = htmlspecialchars(addslashes(trim(mysqli_real_escape_string($conn, $data['password']))));
+    $telp = htmlspecialchars(addslashes(trim(mysqli_real_escape_string($conn, $data['telp']))));
+    $alamat = htmlspecialchars(addslashes(trim(mysqli_real_escape_string($conn, $data['alamat']))));
+
+    $password = password_hash($password, PASSWORD_DEFAULT);
+    $checkEmail = mysqli_query($conn, "SELECT * FROM users WHERE email='$email'");
+    if (mysqli_num_rows($checkEmail) > 0) {
+      $_SESSION["message-danger"] = "Maaf, akun yang anda masukan sudah terdaftar.";
+      $_SESSION["time-message"] = time();
+      return false;
+    } else {
+      
+    }
+  }
+}
+if (isset($_SESSION["data-user"])) {
+  function edit_profile($data)
+  {
+    global $conn, $idUser;
+    $username = htmlspecialchars(addslashes(trim(mysqli_real_escape_string($conn, $data["username"]))));
+    $password = htmlspecialchars(addslashes(trim(mysqli_real_escape_string($conn, $data["password"]))));
+    $password = password_hash($password, PASSWORD_DEFAULT);
+    mysqli_query($conn, "UPDATE users SET username='$username', password='$password' WHERE id_user='$idUser'");
+    return mysqli_affected_rows($conn);
+  }
+  function add_user($data)
+  {
+    global $conn;
+    $username = htmlspecialchars(addslashes(trim(mysqli_real_escape_string($conn, $data["username"]))));
+    $email = htmlspecialchars(addslashes(trim(mysqli_real_escape_string($conn, $data["email"]))));
+    $checkEmail = mysqli_query($conn, "SELECT * FROM users WHERE email='$email'");
+    if (mysqli_num_rows($checkEmail) > 0) {
+      $_SESSION["message-danger"] = "Maaf, email yang anda masukan sudah terdaftar.";
+      $_SESSION["time-message"] = time();
+      return false;
+    }
+    $password = htmlspecialchars(addslashes(trim(mysqli_real_escape_string($conn, $data["password"]))));
+    $password = password_hash($password, PASSWORD_DEFAULT);
+    mysqli_query($conn, "INSERT INTO users(username,email,password) VALUES('$username','$email','$password')");
+    return mysqli_affected_rows($conn);
+  }
+  function edit_user($data)
+  {
+    global $conn, $time;
+    $id_user = htmlspecialchars(addslashes(trim(mysqli_real_escape_string($conn, $data["id-user"]))));
+    $username = htmlspecialchars(addslashes(trim(mysqli_real_escape_string($conn, $data["username"]))));
+    $email = htmlspecialchars(addslashes(trim(mysqli_real_escape_string($conn, $data["email"]))));
+    $emailOld = htmlspecialchars(addslashes(trim(mysqli_real_escape_string($conn, $data["emailOld"]))));
+    if ($email != $emailOld) {
+      $checkEmail = mysqli_query($conn, "SELECT * FROM users WHERE email='$email'");
+      if (mysqli_num_rows($checkEmail) > 0) {
+        $_SESSION["message-danger"] = "Maaf, email yang anda masukan sudah terdaftar.";
+        $_SESSION["time-message"] = time();
+        return false;
+      }
+    }
+    $updated_at = date("Y-m-d " . $time);
+    mysqli_query($conn, "UPDATE users SET username='$username', email='$email', updated_at='$updated_at' WHERE id_user='$id_user'");
+    return mysqli_affected_rows($conn);
+  }
+  function delete_user($data)
+  {
+    global $conn;
+    $id_user = htmlspecialchars(addslashes(trim(mysqli_real_escape_string($conn, $data["id-user"]))));
+    mysqli_query($conn, "DELETE FROM users WHERE id_user='$id_user'");
+    return mysqli_affected_rows($conn);
+  }
+}
---
+++ resources/dash-sidebar.php
@@ -0,0 +1,25 @@
+
+<nav class="sidebar sidebar-offcanvas shadow" style="background-color: rgb(3, 164, 237);" id="sidebar">
+  <ul class="nav">
+    <li class="nav-item">
+      <a class="nav-link" style="cursor: pointer;" onclick="window.location.href='./'">
+        <i class="mdi mdi-grid-large menu-icon"></i>
+        <span class="menu-title">Dashboard</span>
+      </a>
+    </li>
+    <li class="nav-item nav-category">Kelola Pengguna</li>
+    <li class="nav-item">
+      <a class="nav-link" style="cursor: pointer;" onclick="window.location.href='users'">
+        <i class="mdi mdi-account-multiple-outline menu-icon"></i>
+        <span class="menu-title">Users</span>
+      </a>
+    </li>
+    <li class="nav-item nav-category"></li>
+    <li class="nav-item">
+      <a class="nav-link" style="cursor: pointer;" onclick="window.location.href='../auth/signout'">
+        <i class="mdi mdi-logout-variant menu-icon"></i>
+        <span class="menu-title">Keluar</span>
+      </a>
+    </li>
+  </ul>
+</nav>
